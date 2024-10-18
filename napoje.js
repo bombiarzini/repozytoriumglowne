3d3f0f39b8c68e52f9f1546765470316e4739c56
bombiarzini <kacperdragon025@gmail.com>
2024-10-18 09:35:00 +0200

    zadanie napoje

---
+++ napoje.js
@@ -0,0 +1,39 @@
+//task1
+let napoj={
+    nazwa: "Napoj orzezwiajacy",
+    objętość: 500,
+    cena: 3.20
+};
+
+
+//task2
+let napoje=[
+    {  nazwa: "Cisowianka",
+        objętość: 1000,
+        cena: 2.20
+    },
+    {  nazwa: "Oranzada",
+        objętość: 300,
+        cena: 5.30
+    },
+    {  nazwa: "Coca Cola",
+        objętość: 250,
+        cena: 6.90
+    }
+];
+console.log("Dlugosc tablicy: ", napoje.length)
+
+//task3
+
+//task4
+
+napoje.push(napoj)
+console.log(napoje)
+
+//task5
+console.log("___________________________")
+let kopiatablicy =  napoje
+console.log(kopiatablicy)
+console.log("Dlugosc tablicy: ", kopiatablicy.length)
+
+
