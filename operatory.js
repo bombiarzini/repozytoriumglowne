5df6255e91911e9085a71b38b417ea17b70a5369
bombiarzini <kacperdragon025@gmail.com>
2024-11-05 09:03:03 +0100

    operatory

---
+++ operatory.js
@@ -0,0 +1,38 @@
+console.log(2*3+1);
+console.log(2**4);
+console.log(5/1);
+console.log(8**2-5**2);
+
+console.log("_______________________");
+
+console.log(4*5==20);
+console.log(6*5=="30");
+console.log(-17<0);
+console.log(25>1);
+console.log(2+2*2>4);
+
+console.log("_______________________");
+
+console.log(true!=false);
+console.log(false==false);
+console.log(false+false!=true);
+console.log(true&&false==false&&true);
+
+console.log("_______________________");
+
+let a=1;
+let b=2;
+let c;
+c=a+b;
+console.log(c);
+
+console.log("_______________________");
+
+let p=-1;
+let r=2;
+
+if(p>0 && r>0){
+    console.log("Obie liczby sa dodatnie");
+}else{
+    console.log("Ktoras z liczb nie jest dodatnia");
+}
