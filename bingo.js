0478d1a00a43fc966aa1455656990dd9aa80aca6
bombiarzini <kacperdragon025@gmail.com>
2024-11-19 09:33:34 +0100

    zalegle

---
+++ bingo.js
@@ -0,0 +1,8 @@
+let liczba=111;
+
+if(liczba>90 && liczba<110){
+    console.log("Bingo!");
+}else{
+    console.log("Pudło");
+}
+console.log("_____________________________");
---
+++ objetosc.js
@@ -0,0 +1,6 @@
+bok1=prompt("Podaj ile wynosi pierwszy bok: ");
+bok2=prompt("Podaj ile wynosi drugi bok: ");
+wysokosc=prompt("Podaj ile wynosi wysokosc: ");
+
+let objetosc=bok1*bok2*wysokosc;
+console.log("Objetosc wynosi: ", objetosc);
